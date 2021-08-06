9d7ab362b673057c0e7ebfc92413b6103b137f9a
Juan Alvarez <juan.alvarez@t-evolvers.com>
2021-08-06 15:27:22 -0500

    Cambios en swipe y page object

--- Appium/UnitTest2.cs
+++ Appium/UnitTest2.cs
@@ -1,16 +1,17 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.MultiTouch;
 using System;
 using System.Threading;
 using OpenQA.Selenium.Appium;
-
+using Appium.src;
 namespace Appium
 {
     public class UnitTest2
     {
         AppiumOptions options;
-        AppiumDriver<AndroidElement> driver;
+        AndroidDriver<AppiumWebElement> driver;
 
         [SetUp]
         public void Setup()
@@ -20,23 +21,47 @@ namespace Appium
             options.AddAdditionalCapability("platformVersion", "11");
             options.AddAdditionalCapability("deviceName", "Mi 10T Pro");
             options.AddAdditionalCapability("automationName", "Appium");
-            options.AddAdditionalCapability("appPackage", "com.google.android.apps.translate");
-            options.AddAdditionalCapability("appActivity", "com.google.android.apps.translate.TranslateActivity");
-            options.AddAdditionalCapability("noReset", "false");
-            driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), options);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-
+            options.AddAdditionalCapability("appPackage", "com.google.android.apps.messaging");
+            options.AddAdditionalCapability("appActivity", "com.google.android.apps.messaging.ui.ConversationListActivity");
+            options.AddAdditionalCapability("noReset", "true");
+            driver = new AndroidDriver<AppiumWebElement>(new Uri("http://localhost:4723/wd/hub"), options);
         }
 
 
         [Test]
         public void Test1()
         {
-            driver.FindElement(By.XPath("//*[@text='Traducir sin conexión']")).Click();
-            driver.FindElement(By.XPath("//*[@text='LISTO']")).Click();
-            driver.FindElement(By.XPath("//*[@text='Configuración']")).Click();
-            Console.WriteLine(driver.FindElement(By.Id("android:id/summary")).GetAttribute("text"));
+            MensajesChat mensajesChat = new MensajesChat(driver);
+            mensajesChat.clicIniciarChat();
+            mensajesChat.EsconderChat();
 
+           
+
+            bool encontroContacto = false;
+            do
+            {
+                try
+                {
+                    encontroContacto = driver.FindElement(By.XPath("//*[@text='An Hector Fabio Devot Celsia']")).Displayed;
+                }
+                catch(Exception e)
+                {
+
+                }
+                
+                Swipe();
+
+            } while (encontroContacto == false);
+
+        }
+
+        public void Swipe()
+        {
+            
+
+            TouchAction touchAction = new TouchAction(driver);
+            touchAction.Press(500, 2148)
+                .MoveTo(500, 400).Release().Perform();
         }
 
         [TearDown]
---
+++ Appium/src/MensajesChat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using SeleniumExtras.PageObjects;
+namespace Appium.src
+{
+    public class MensajesChat
+    {
+        [FindsBy(How = How.XPath, Using = "//android.widget.Button[@content-desc='Iniciar chat']")]
+        IWebElement iniciarChat;
+        AndroidDriver<AppiumWebElement> driver;
+        public MensajesChat(AndroidDriver<AppiumWebElement> driver)
+        {
+            PageFactory.InitElements(driver,this);
+            this.driver = driver;
+        }
+
+        public void clicIniciarChat()
+        {
+            iniciarChat.Click();
+        }
+
+        public void EsconderChat()
+        {
+            driver.HideKeyboard();
+        }
+    }
+}
